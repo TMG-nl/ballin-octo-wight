581979200f348bcb92e9f0cfb5bd6e47b82d98e4
Ifthikhan Nazeem <iftecan2000@gmail.com>
2012-06-07 18:24:19 +0200

    Integrated league and knockout to work together.

--- knockout.pl
+++ knockout.pl
@@ -1,14 +1,11 @@
-round_1([[a,b],[c,d],[e,f],[g,h]]).
+matched_played_knockout(d,b,2,3).
+matched_played_knockout(h,f,5,3).
+matched_played_knockout(l,j,2,3).
+matched_played_knockout(p,n,6,3).
 
-match_played(a,b,2,3).
-match_played(c,d,5,3).
-match_played(e,f,2,3).
-match_played(g,h,6,3).
-
-match_played(b,c,6,3).
-match_played(f,g,5,2).
-match_played(b,f,1,2).
-
+matched_played_knockout(b,h,6,3).
+matched_played_knockout(j,p,5,2).
+matched_played_knockout(b,j,1,2).
 
 next_round([], []).
 next_round([[T1,T2], [N1,N2] | Rest], NextRound) :- next_round(Rest, NextRoundRest),
@@ -19,5 +16,5 @@ next_round([[T1,T2], [N1,N2] | Rest], NextRound) :- next_round(Rest, NextRoundRe
 winner([[A, B]], C) :- winnerOf(A, B, C).
 winner(CurrentRound, Winner) :- next_round(CurrentRound, NextRound), winner(NextRound, Winner).
 
-winnerOf(T1, T2, T1) :- match_played(T1, T2, S1, S2), S1>S2.
-winnerOf(T1, T2, T2) :- match_played(T1, T2, S1, S2), S2>S1.
+winnerOf(T1, T2, T1) :- matched_played_knockout(T1, T2, S1, S2), S1>S2.
+winnerOf(T1, T2, T2) :- matched_played_knockout(T1, T2, S1, S2), S2>S1.
--- league.pl
+++ league.pl
@@ -10,14 +10,10 @@ insert(X,[],[X]).
 sum([], 0).
 sum([H|T], S) :- sum(T,H1), S is H1+H.
 
-
-
-
-
 more_points(X,Y) :- points(X,P1), points(Y,P2), P1>=P2.
 
 
-ranking(Result) :- teams(X),insert_sort(X, Result).
+ranking(League, Result) :- insert_sort(League, Result).
 
 
 points_for_match(T1, T2, 3) :- match_played(T1, T2, S1, S2), S1>S2;
@@ -27,15 +23,21 @@ points_for_match(T1, T2, 1) :- match_played(T1, T2, S1, S2), S1=S2;
 points_for_match(T1, T2, 0) :- match_played(T1, T2, S1, S2), S1<S2;
 			       match_played(T2, T1, S2, S1), S1<S2.
 
-
 points(Team, Points):- findall(P, points_for_match(Team,_,P), Y), sum(Y, Points).
 
 
+group_phase([], []).
+group_phase([FirstLeague | Rest], Winners) :- group_phase(Rest, NextWinners), 
+					ranking(FirstLeague, [First, Second | _]),
+					Winners = [[First, Second] | NextWinners].
 
 
-
-
-teams([a,b,c,d]).
+leagues([
+[a, b, c, d],
+[e, f, g, h],
+[i, j, k, l],
+[m, n, o, p]
+]).
 
 match_played(a,b,2,3).
 match_played(a,c,2,4).
@@ -44,6 +46,26 @@ match_played(b,c,4,3).
 match_played(b,d,3,3).
 match_played(c,d,5,7).
 
+match_played(e,f,2,3).
+match_played(e,g,2,4).
+match_played(e,h,2,5).
+match_played(f,g,4,3).
+match_played(f,h,3,3).
+match_played(g,h,5,7).
+
+match_played(i,j,2,3).
+match_played(i,k,2,4).
+match_played(i,l,2,5).
+match_played(j,k,4,3).
+match_played(j,l,3,3).
+match_played(k,l,5,7).
+
+match_played(m,n,2,3).
+match_played(m,o,2,4).
+match_played(m,p,2,5).
+match_played(n,o,4,3).
+match_played(n,p,3,3).
+match_played(o,p,5,7).
 
 
 :- points_for_match(d,b,1).
