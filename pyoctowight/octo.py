99d88e9782d30c7f11b1acd074b65773498515cb
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2012-06-08 13:09:58 +0200

    Responding group_phase post with group phase winners

--- pyoctowight/octo.py
+++ pyoctowight/octo.py
@@ -10,40 +10,37 @@ import os
 import subprocess
 import json
 import re
+import tempfile
 
 from flask import Flask
 from flask import request, render_template
 
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    # Coming...
-    #return render_template('index.html')
-    return 'Ballin Octo Wight!'
+    return render_template('index.html')
 
 @app.route('/group_phase', methods=['POST'])
 def group_phase():
     leagues = request.json['leagues']
     matches = request.json['matches']
-
     all_leagues = [leagues[group] for group in sorted(leagues)]
     matches_played = [
         "match_played({}, {}, {}, {}).".format(match['team1'].lower(), match['team2'].lower(),
                                                match['score1'], match['score2'])
         for match in matches]
-    data = """
-    leagues({}).
+    data = """leagues({}).
     {}
     """.format(prologify_json(json.dumps(all_leagues)),
                "\n".join(matches_played))
 
     fh, datafile = tempfile.mkstemp(prefix='ballinoctowight-', suffix='.pl')
-    close(fh)
-    with open(datafile, r) as f:
+    os.close(fh)
+    with open(datafile, "w") as f:
         f.write(data)
-    #os.unlink(datafile)
+    os.unlink(datafile)
 
     league_exec = os.path.join(BASEDIR, 'league.pl')
     winners = subprocess.check_output(
@@ -56,16 +53,12 @@ def jsonify_prolog(string):
     '''Makes a prolog output string valid JSON'''
     # [[d,b],[h,f],[l,j],[p,n]] -> [["d","b"],["h","f"],["l","j"],["p","n"]]
     json_string = re.sub(r'\b(\w+)\b', r'"\1"', string, flags=re.I)
-    print string
-    print json_string
     return json_string
 
 def prologify_json(string):
     '''Makes a JSON string valid prolog'''
     # [["D","B"],["H","F"],["L","J"],["P","N"]] -> [[d,b],[h,f],[l,j],[p,n]]
-    prolog_string = re.sub(r'\b"(\w+)"\b', lambda m: m.groups(1).lower(), string, flags=re.I)
-    print string
-    print prolog_string
+    prolog_string = re.sub(r'"(\w+)"', lambda m: m.group(1).lower(), string, flags=re.I)
     return prolog_string
 
 
