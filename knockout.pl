76b43a2322e659a11e66c9bd0b1a9bc9f0097282
Rodrigo Bernardo Pimentel <rbp@isnomore.net>
2012-06-08 10:39:59 +0200

    Commented out Prolog test data

--- knockout.pl
+++ knockout.pl
@@ -1,12 +1,3 @@
-matched_played_knockout(d,b,2,3).
-matched_played_knockout(h,f,5,3).
-matched_played_knockout(l,j,2,3).
-matched_played_knockout(p,n,6,3).
-
-matched_played_knockout(b,h,6,3).
-matched_played_knockout(j,p,5,2).
-matched_played_knockout(b,j,1,2).
-
 next_round([], []).
 next_round([[T1,T2], [N1,N2] | Rest], NextRound) :- next_round(Rest, NextRoundRest),
 					 	    winnerOf(T1, T2, T3),
@@ -18,3 +9,14 @@ winner(CurrentRound, Winner) :- next_round(CurrentRound, NextRound), winner(Next
 
 winnerOf(T1, T2, T1) :- matched_played_knockout(T1, T2, S1, S2), S1>S2.
 winnerOf(T1, T2, T2) :- matched_played_knockout(T1, T2, S1, S2), S2>S1.
+
+
+%% Test data
+% matched_played_knockout(d,b,2,3).
+% matched_played_knockout(h,f,5,3).
+% matched_played_knockout(l,j,2,3).
+% matched_played_knockout(p,n,6,3).
+
+% matched_played_knockout(b,h,6,3).
+% matched_played_knockout(j,p,5,2).
+% matched_played_knockout(b,j,1,2).
--- league.pl
+++ league.pl
@@ -1,7 +1,7 @@
 insert_sort(List,Sorted):-i_sort(List,[],Sorted).
 i_sort([],Acc,Acc).
 i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
-   
+
 insert(X,[Y|T],[Y|NT]):-not(more_points(X,Y)),insert(X,T,NT).
 insert(X,[Y|T],[X,Y|T]):-more_points(X,Y).
 insert(X,[],[X]).
@@ -32,43 +32,44 @@ group_phase([FirstLeague | Rest], Winners) :- group_phase(Rest, NextWinners),
 					Winners = [[First, Second] | NextWinners].
 
 
-leagues([
-[a, b, c, d],
-[e, f, g, h],
-[i, j, k, l],
-[m, n, o, p]
-]).
+%% Test data
+% leagues([
+% [a, b, c, d],
+% [e, f, g, h],
+% [i, j, k, l],
+% [m, n, o, p]
+% ]).
 
-match_played(a,b,2,3).
-match_played(a,c,2,4).
-match_played(a,d,2,5).
-match_played(b,c,4,3).
-match_played(b,d,3,3).
-match_played(c,d,5,7).
+% match_played(a,b,2,3).
+% match_played(a,c,2,4).
+% match_played(a,d,2,5).
+% match_played(b,c,4,3).
+% match_played(b,d,3,3).
+% match_played(c,d,5,7).
 
-match_played(e,f,2,3).
-match_played(e,g,2,4).
-match_played(e,h,2,5).
-match_played(f,g,4,3).
-match_played(f,h,3,3).
-match_played(g,h,5,7).
+% match_played(e,f,2,3).
+% match_played(e,g,2,4).
+% match_played(e,h,2,5).
+% match_played(f,g,4,3).
+% match_played(f,h,3,3).
+% match_played(g,h,5,7).
 
-match_played(i,j,2,3).
-match_played(i,k,2,4).
-match_played(i,l,2,5).
-match_played(j,k,4,3).
-match_played(j,l,3,3).
-match_played(k,l,5,7).
+% match_played(i,j,2,3).
+% match_played(i,k,2,4).
+% match_played(i,l,2,5).
+% match_played(j,k,4,3).
+% match_played(j,l,3,3).
+% match_played(k,l,5,7).
 
-match_played(m,n,2,3).
-match_played(m,o,2,4).
-match_played(m,p,2,5).
-match_played(n,o,4,3).
-match_played(n,p,3,3).
-match_played(o,p,5,7).
+% match_played(m,n,2,3).
+% match_played(m,o,2,4).
+% match_played(m,p,2,5).
+% match_played(n,o,4,3).
+% match_played(n,p,3,3).
+% match_played(o,p,5,7).
 
 
-:- points_for_match(d,b,1).
-:- points_for_match(a,b,0).
-:- points_for_match(b,a,3).
-:- points_for_match(b,d,1).
+% :- points_for_match(d,b,1).
+% :- points_for_match(a,b,0).
+% :- points_for_match(b,a,3).
+% :- points_for_match(b,d,1).
